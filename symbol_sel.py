56a846bd34bc56e260acfdf743d8b87373ff1b74
leo <leo.wang@monash.edu>
2021-06-20 22:06:47 +1000

    minor changes

--- symbol_sel.py
+++ symbol_sel.py
@@ -1,14 +1,15 @@
 from binance.client import Client
 from utils import save_symbols
 
-client = Client()
-info = client.futures_exchange_info()
-symbols_info = info['symbols']
-symbols = [
-    s['symbol'] for s in symbols_info if
-    s['status'] == 'TRADING' and
-    s['contractType'] == 'PERPETUAL' and
-    s['symbol'].endswith('USDT')
-]
-save_symbols(symbols)
-print(f'{len(symbols)} symbols have ben selected and saved to symbols.txt')
+if __name__ == '__main__':
+    client = Client()
+    info = client.futures_exchange_info()
+    symbols_info = info['symbols']
+    symbols = [
+        s['symbol'] for s in symbols_info if
+        s['status'] == 'TRADING' and
+        s['contractType'] == 'PERPETUAL' and
+        s['symbol'].endswith('USDT')
+    ]
+    save_symbols(symbols)
+    print(f'{len(symbols)} symbols have ben selected and saved to symbols.txt')
